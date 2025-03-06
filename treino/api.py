196472efc0142ffff6d0598c3440e1115e76d264
“mtstechnologies” <michael.trindade@hotmail.com>
2025-03-06 11:03:50 -0400

    Config. rota criar_aluno para receber os dados da URI e salvar no banco.

--- treino/api.py
+++ treino/api.py
@@ -1,7 +1,28 @@
 from ninja import Router
 
+from treino.models import Alunos
+
+from .schemas import AlunosSchema
+
 treino_router = Router()
 
-@treino_router.get('teste/')
-def criar_aluno(request):
-    return{'Ola mundo':'Ola mundo'}
+@treino_router.post('', response={201: AlunosSchema})
+def criar_aluno(request, aluno_schema: AlunosSchema):
+    #recebendo os dados 
+    nome = aluno_schema.dict()['nome']
+    email = aluno_schema.dict()['email']
+    faixa = aluno_schema.dict()['faixa']
+    data_nascimento = aluno_schema.dict()['data_nascimento']
+
+    #Recebendo: temos uma opcao mais simplificado de fazer a mesma coisa de descompressao de dados.
+    #nome, email, faixa, data_nascimento = **aluno_schema.dict()
+    
+    #Salvando: os dados na model Alunos de maneira simples: aluno = Alunos(**aluno_schema.dict())
+    aluno = Alunos(nome=nome,
+                   email=email,
+                   faixa=faixa,
+                   data_nascimento=data_nascimento)
+    aluno.save()
+
+
+    return aluno
