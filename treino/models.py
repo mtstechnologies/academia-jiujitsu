dbf1492a8d57f3460a03ec4e936f8300af20e203
“mtstechnologies” <michael.trindade@hotmail.com>
2025-03-05 22:47:47 -0400

    Model - Criando primeira classe Aluno

--- treino/models.py
+++ treino/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Alunos(models.Model):
+    faixa_choices = (
+        ('B', 'Branca'),
+        ('A', 'Azul'),
+        ('R', 'Roxa'),
+        ('M', 'Marrom'),
+        ('P', 'Preta')
+    )
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    faixa = models.CharField(max_length=1, choices=faixa_choices, default='B')
+
+    def __str__(self):
+        return self.nome
